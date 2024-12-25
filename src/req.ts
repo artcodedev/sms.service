3ceebb83ecef985a6387da8e869cbb8f261fb447
art[code] <you@example.com>
2024-12-25 18:04:32 +1000

    12

--- src/req.ts
+++ src/req.ts
@@ -11,7 +11,7 @@ import { TTYGetter } from "./Utils/TTYGetter";
 
     // const token: string = await Token.sign({uid: 'uid'}, SecretKey.secret_key_micro, 1211);
 
-    // const response = await fetch("http://localhost:3005/getalltty", {
+    // const response = await fetch("http://localhost:3005/getallmessages", {
     //     method: "POST",
     //     body: JSON.stringify({token: token, phone: "+79020542692", port: "/dev/ttyUSB0"}),
     //     headers: { "Content-Type": "application/json" },
@@ -21,7 +21,7 @@ import { TTYGetter } from "./Utils/TTYGetter";
 
     // console.log(html)
 
-    // const token: string = await Token.sign({uid: 'uid'}, SecretKey.secret_key_micro, 1211);
+    const token: string = await Token.sign({uid: 'uid'}, SecretKey.secret_key_micro, 1211);
 
     // const data = {
     //     token: token,
@@ -33,17 +33,28 @@ import { TTYGetter } from "./Utils/TTYGetter";
     //     phone: "+79020542692"
     // }
 
-    // const response = await fetch("http://localhost:3006/micro/withdraw/sberbank_rub", {
-    //     method: "POST",
-    //     body: JSON.stringify(data),
-    //     headers: { "Content-Type": "application/json" },
-    // });
 
-    // const html = await response.text();
+    const data = {
+        token: token,
+        login: 'SFkdjsdfsdfksdfkl',
+        pass: 'kalogen777!!!--F',
+        number_card: '2202206759545713',
+        id_card: 1,
+        uid_bank: 'sdsdsdsdsddsdsd'
+    }
 
-    // // const html = await response.json();
+    const response = await fetch("http://localhost:3006/micro/amount/sberbank_rub", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" },
+    });
 
-    // console.log(html)
+    const html = await response.text();
+
+    // const html = await response.json();
+
+    console.log(html)
+
 
 
 
@@ -57,70 +68,70 @@ import { TTYGetter } from "./Utils/TTYGetter";
 
 
 
-    const data = [
-        {
-            id: "37",
-            phone: "900",
-            date: "\"24/12/24",
-            message: "Вход в СберБанк Онлайн в 13:39 по московскому времени. Если входили",
-          },
-          {
-            id: "38",
-            phone: "900",
-            date: "\"24/12/24",
-            message: "Вход в СберБанк Онлайн в 13:40 по московскому времени. Если входили",
-          },
-          {
-            id: "39",
-            phone: "900",
-            date: "\"24/12/24",
-            message: " не вы, позвоните на 900.",
-          }, {
-            id: "40",
-            phone: "900",
-            date: "\"24/12/24",
-            message: "Подтвердите перевод с Плат.Счёта  *8005 на карту MIR3334 на сумму 1",
-          },
-          {
-            id: "41",
-            phone: "900",
-            date: "\"24/12/24",
-            message: "0р. Код: 07271. Никому его не сообщайте и не подтверждайте операции",
-          },
-          {
-            id: "42",
-            phone: "900",
-            date: "\"24/12/24",
-            message: ", которые вы не совершали.",
-          }
-    ]
+    // const data = [
+    //     {
+    //         id: "37",
+    //         phone: "900",
+    //         date: "\"24/12/24",
+    //         message: "Вход в СберБанк Онлайн в 13:39 по московскому времени. Если входили",
+    //       },
+    //       {
+    //         id: "38",
+    //         phone: "900",
+    //         date: "\"24/12/24",
+    //         message: "Вход в СберБанк Онлайн в 13:40 по московскому времени. Если входили",
+    //       },
+    //       {
+    //         id: "39",
+    //         phone: "900",
+    //         date: "\"24/12/24",
+    //         message: " не вы, позвоните на 900.",
+    //       }, {
+    //         id: "40",
+    //         phone: "900",
+    //         date: "\"24/12/24",
+    //         message: "Подтвердите перевод с Плат.Счёта  *8005 на карту MIR3334 на сумму 1",
+    //       },
+    //       {
+    //         id: "41",
+    //         phone: "900",
+    //         date: "\"24/12/24",
+    //         message: "0р. Код: 07271. Никому его не сообщайте и не подтверждайте операции",
+    //       },
+    //       {
+    //         id: "42",
+    //         phone: "900",
+    //         date: "\"24/12/24",
+    //         message: ", которые вы не совершали.",
+    //       }
+    // ]
 
-    const num: string[] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    // const num: string[] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-    const code_sms: string[] = []
+    // const code_sms: string[] = []
 
-    for (let i of data.reverse()) {
+    // for (let i of data.reverse()) {
 
-        const message: string = i.message.toLowerCase();
+    //     const message: string = i.message.toLowerCase();
 
-        if (message.includes("код")) {
-            const position = message.indexOf('код');
+    //     if (message.includes("код")) {
+    //         const position = message.indexOf('код');
 
-            for (let p = position; p < message.length; p++) {
+    //         for (let p = position; p < message.length; p++) {
 
-                if (num.includes('.')) {
-                    break
-                }
+    //             if (num.includes('.')) {
+    //                 break
+    //             }
 
-                if (num.includes(message[p])) {
-                    code_sms.push(message[p])
-                }
-            }
+    //             if (num.includes(message[p])) {
+    //                 code_sms.push(message[p])
+    //             }
+    //         }
 
-        }
-    }
+    //     }
+    // }
 
-    console.log(code_sms.join(''))
+    // console.log(code_sms.join(''))
 
     // const token: string = await Token.sign({uid: 'uid'}, SecretKey.secret_key_micro, 1211);
 
